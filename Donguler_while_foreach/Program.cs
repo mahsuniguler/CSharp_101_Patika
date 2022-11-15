2704ac03b0275ee50215d6ee96d958da87e133d6
Mustafa Mahsuni Güler <m.mahsuni.glr@gmail.com>
2022-11-15 13:00:28 +0300

    donguler_while_foreach

---
+++ Donguler_while_foreach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            // 1 den başlayarak girilen sayıya kadar Sayıların Ortalamasını Yazdırmak
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac=1,toplam=0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            int ortalama = toplam/sayi;
+            Console.WriteLine(ortalama);
+
+            // a dan z ye kadar tüm harfleri yazdır
+            char character ='a';
+            while (character<='z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            Console.ReadLine();
+            Console.WriteLine("****** Foreach ******");
+            string[] arabalar = {"BMW","Mercedes","Toyota","Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
